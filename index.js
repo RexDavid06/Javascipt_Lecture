09e43513de46092a7c2062d9d54bbdd74639d560
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2024-12-10 13:40:07 +0100

    Javascript Journey

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello world")
+
+let num = 4 + 6
+console.log(num)
+
+let username = 'Rex-David'
+console.log(typeof username)  
+
+const pi = 3.12
+console.log(pi)
+
+num = 200
+console.log(typeof num)
+
